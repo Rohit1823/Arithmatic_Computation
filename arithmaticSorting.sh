13a8e2c9fcc3f866c95219afb0ef37c99b2d60ef
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-16 09:12:33 +0530

    ADDED : Taking three inputs

---
+++ arithmaticSorting.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Problem Statement : Arithmatic Computation and Sorting
+
+#Name : Rohit Machale
+
+read -p "Enter First value: " input1
+read -p "Enter Second value: " input2
+read -p "Enter Third value: " input3
+
